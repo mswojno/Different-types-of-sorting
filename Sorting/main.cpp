78429bb40fb58ef5e60ef9e7d12b2a83e8486f5a
MATTHEW S WOJNO <MatthewSWojno@MATTHEWs-MacBook-Pro.local>
2015-01-06 09:16:11 -0500

    Initial Commit

---
+++ Sorting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sorting
+//
+//  Created by MATTHEW S WOJNO on 1/6/15.
+//  Copyright (c) 2015 Matthew Wojno. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
